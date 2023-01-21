00a9b981ac845aa2a2d70a8f31b7ea9b6bca0a85
Ferdinand Cuisin <ferdinand.c@meta-api.io>
2023-01-21 23:50:36 +0100

    feat: add tabs with search bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,35 @@
 import { MantineProvider, MantineThemeOverride } from "@mantine/core";
 import type { AppProps } from "next/app";
+import "remixicon/fonts/remixicon.css";
 
 const MantineThemeProps: MantineThemeOverride = {
   fontFamily: "Greycliff CF, sans-serif",
   primaryShade: 7,
   fontSizes: {
-    xs: 10,
-    sm: 12,
-    md: 14,
-    lg: 16,
-    xl: 20,
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
   },
-  headings: { fontFamily: "Gill Sans, sans-serif" },
+  headings: { fontFamily: "Gill Sans, sans-serif", fontWeight: 600 },
+  cursorType: "pointer",
+  colors: {
+    green: [
+      "#DAE2B6",
+      "#8FCFB2",
+      "#9AE6B4",
+      "#EAEAEA",
+      "#47BB78",
+      "#37A169",
+      "#2F8559",
+      "#276748",
+      "#21543D",
+      "#1C4531",
+    ],
+  },
+  white: "#F6F6F6",
+  black: "#1E1E1E",
 };
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,23 @@
 import {
   Container,
-  Stack,
   Title,
   createStyles,
   Group,
   Autocomplete,
+  Tabs,
+  ActionIcon,
 } from "@mantine/core";
 import Image from "next/image";
-import AddPlantModal from "../components/AddPlantModal";
 
 import Logo from "../components/images/main-logo.svg";
+import { HEADER_HEIGHT } from "../components/utils/constants";
 
 const useStyles = createStyles((theme) => ({
   highlight: {
     position: "relative",
-    backgroundColor: theme.colors.green[5],
-    borderRadius: theme.radius.sm,
+    color: theme.white,
+    backgroundColor: theme.colors.green[7],
+    borderRadius: theme.radius.md,
     padding: "4px 12px",
   },
 }));
@@ -24,30 +26,82 @@ export default function Home() {
   const { classes } = useStyles();
 
   return (
-    <Container p={0}>
-      <Stack
-        align="center"
-        p={80}
-        sx={(theme) => ({
-          position: "relative",
-          borderRadius: 50,
-          backgroundColor: theme.colors.green[3],
-        })}
+    <Container py={0} px={10}>
+      <Group
+        spacing={0}
+        position="center"
+        style={{ height: HEADER_HEIGHT }}
+        my="lg"
+        noWrap
       >
         <Image src={Logo} height="50" width="100" alt="main-logo" />
-        <Title weight="bold">
+        <Title order={2}>
           <span className={classes.highlight}>Plant</span> Watering Tracker
         </Title>
-        <Group style={{ position: "absolute", bottom: -18 }}>
-          <Autocomplete
-            size="md"
-            radius="md"
-            placeholder="Search..."
-            data={[]}
-          />
-          <AddPlantModal />
-        </Group>
-      </Stack>
+      </Group>
+      <Group position="apart">
+        <Tabs
+          defaultValue="gallery"
+          variant="pills"
+          color="green.6"
+          styles={(theme) => ({
+            root: { width: "100%" },
+            tab: {
+              border: `1px solid ${theme.colors.green[7]}`,
+              color: theme.colors.green[7],
+            },
+            tabLabel: { fontWeight: 600 },
+          })}
+        >
+          <Tabs.List>
+            <Group position="apart" style={{ width: "100%" }}>
+              <Group>
+                <Tabs.Tab
+                  value="gallery"
+                  icon={<i className="ri-plant-line ri-lg"></i>}
+                >
+                  Today
+                </Tabs.Tab>
+                <Tabs.Tab
+                  value="messages"
+                  icon={<i className="ri-calendar-line ri-lg"></i>}
+                >
+                  Upcoming
+                </Tabs.Tab>
+              </Group>
+              <Group>
+                <Autocomplete
+                  placeholder="search"
+                  data={[]}
+                  styles={(theme) => ({
+                    input: {
+                      border: 0,
+                      borderRadius: 0,
+                      borderBottom: `1px solid ${theme.colors.green[7]}`,
+                    },
+                  })}
+                  icon={<i className="ri-search-line ri-lg"></i>}
+                />
+                <ActionIcon
+                  color="dark"
+                  variant="filled"
+                  style={{ height: 36, width: 36 }}
+                >
+                  <i className="ri-add-line ri-lg"></i>
+                </ActionIcon>
+              </Group>
+            </Group>
+          </Tabs.List>
+
+          <Tabs.Panel value="gallery" pt="xs">
+            Today plants
+          </Tabs.Panel>
+
+          <Tabs.Panel value="messages" pt="xs">
+            Upcoming plants
+          </Tabs.Panel>
+        </Tabs>
+      </Group>
     </Container>
   );
 }
